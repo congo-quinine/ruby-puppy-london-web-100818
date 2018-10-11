220650ce69e765aaf25b08d5509a77a664527a99
Rob Parker <into.the.parker@gmail.com>
2018-10-11 16:48:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,3 @@
-
-#   describe "Dog class variables" do
-#     it "has a class variable, @@all, the points to an array" do
-#       expect(Dog.class_variable_get(:@@all)).to be_a(Array)
-#     end
-#   end
-
 class Dog 
   
   @@all = []
@@ -25,32 +18,3 @@ class Dog
   end  
   
 end  
-
-#   describe ".clear_all" do
-#     it "is a class method that empties the @@all array of all existing dogs" do
-#       Dog.clear_all
-#       expect(Dog.class_variable_get(:@@all)).to match([]  )
-#     end
-#   end
-
-#   describe ".all" do
-#     it "is a class method that puts out the name of each dog to the terminal" do
-#       expect{Dog.all}.to output("Pluto\nFido\nMaddy\n").to_stdout
-#     end
-#   end
-
-#   describe ".new" do
-#     it "initializes with an argument of a name" do
-#       expect{Dog.new("Snoopy")}.to_not raise_error
-#     end
-#     it "adds the new dog to the @@all array" do
-#       expect(Dog.class_variable_get(:@@all)).to match([pluto, fido, maddy])
-#     end
-#   end
-
-#   describe "#name" do
-#     it "has a name" do
-#       expect(pluto.name).to eq("Pluto")
-#     end
-#   end
-# end
